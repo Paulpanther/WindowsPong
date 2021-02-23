2299984854149c84828a0e61d59a315311e8c461
Paul Methfessel <paul.methfessel@web.de>
2021-02-23 14:25:45 +0100

    Update gradle for release

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.4.30"
+    id("com.github.johnrengelman.shadow") version "4.0.4"
 }
 
 group = "com.paulmethfessel"
@@ -14,6 +16,15 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
+}
+
+tasks {
+    named<ShadowJar>("shadowJar") {
+        mergeServiceFiles()
+        manifest {
+            attributes(mapOf("Main-Class" to "com.paulmethfessel.windowspong.GameKt"))
+        }
+    }
 }
 
 tasks.withType<KotlinCompile> {
